2bbf669ced898678c32bdf1b3bd2eb0409556316
Sethu <sethuramanv23@gmail.com>
2023-08-26 16:10:06 -0400

    Dune quotes

---
+++ QuotesExtension/js/extension-script.js
@@ -0,0 +1,88 @@
+var quotes = [
+
+        //<-------------STAR WARS------------->
+        ['images/yoda.png', 'May the force be with you!'],
+        ['images/quigonn.jpeg', 'The ability to speak does not make you intelligent!'],
+
+        //<-------------HARRY POTTER------------->
+
+
+        //<-------------LOTR------------->
+
+
+        //<-------------DUNE------------->
+        ['images/dune.jpeg', 'I must not fear. Fear is the mind-killer. Fear is the little-death that brings total obliteration. I will face my fear. I will permit it to pass over me and through me. And when it has gone past I will turn the inner eye to see its path. Where the fear has gone there will be nothing. Only I will remain.'],
+        ['images/dune.jpeg', "The mystery of life isn't a problem to solve, but a reality to experience."],
+        ['images/dune.jpeg', "Deep in the human unconscious is a pervasive need for a logical universe that makes sense. But the real universe is always one step beyond logic."],
+        ['images/dune.jpeg', "What do you despise? By this are you truly known."],
+        ['images/dune.jpeg', "There is no escape—we pay for the violence of our ancestors."],
+        ['images/dune.jpeg', "Hope clouds observation."],
+        ['images/dune.jpeg', "The mind commands the body and it obeys. The mind orders itself and meets resistance."],
+        ['images/dune.jpeg', "It is impossible to live in the past, difficult to live in the present and a waste to live in the future."],
+        ['images/dune.jpeg', "The mind can go either direction under stress—toward positive or toward negative: on or off. Think of it as a spectrum whose extremes are unconsciousness at the negative end and hyperconsciousness at the positive end. The way the mind will lean under stress is strongly influenced by training."],
+        ['images/dune.jpeg', "A process cannot be understood by stopping it. Understanding must move with the flow of the process, must join it and flow with it."],
+        ['images/dune.jpeg', "Once men turned their thinking over to machines in the hope that this would set them free. But that only permitted other men with machines to enslave them"],
+        ['images/dune.jpeg', "Fear is the mind-killer."],
+        ['images/dune.jpeg', "A world is supported by four things... ...the learning of the wise, the justice of the great, the prayers of the righteous and the valor of the brave. But all of these things are as nothing without a ruler who knows the art of ruling. Make that the science of your tradition!"],
+        ['images/dune.jpeg', "The Fremen were supreme in that quality the ancients called 'spannungsbogen' -- which is the self-imposed delay between desire for a thing and the act of reaching out to grasp that thing"],
+        ['images/dune.jpeg', "Highly organized research is guaranteed to produce nothing new."],
+        ['images/dune.jpeg', "Any road followed precisely to its end leads precisely nowhere. Climb the mountain just a little bit to test that it's a mountain. From the top of the mountain, you cannot see the mountain."],
+        ['images/dune.jpeg', "Give as few orders as possible, Once you've given orders on a subject, you must always give orders on that subject."],
+        ['images/dune.jpeg', "If you rely only on your eyes, your other senses weaken."],
+        ['images/dune.jpeg', "A ruler must learn to persuade and not to compel."],
+        ['images/dune.jpeg', "A popular man arouses the jealousy of the powerful."],
+        ['images/dune.jpeg', "The power to destroy a thing is the absolute control over it."],
+        ['images/dune.jpeg', "What is important for a leader is that which makes him a leader. It is the needs of his people."],
+        ['images/dune.jpeg', "Science is made up of so many things that appear obvious after they are explained."],
+        ['images/dune.jpeg', "Desperate people are the most dangerous."],
+        ['images/dune.jpeg', "What is the son but an extension of the father?"],
+        ['images/dune.jpeg', "Greatness is a transitory experience. It is never consistent."],
+        ['images/dune.jpeg', "The most persistent principles of the universe were accident and error."],
+        
+
+
+
+        //<-------------OTHERS------------->
+
+        ['images/animalfarm.jpeg', "All animals are equal, but some animals are more equal than others."],
+
+        //<-------------ALCHEMIST------------->
+        ['images/alchemist.jpeg', "When you want something, all the universe conspires in helping you to achieve it."],
+        ['images/alchemist.jpeg', "It's the possibility of having a dream come true that makes life interesting."],
+        ['images/alchemist.jpeg', "When we love, we always strive to become better than we are. When we strive to become better than we are, everything around us becomes better too."],
+        ['images/alchemist.jpeg', "One is loved because one is loved. No reason is needed for loving."],
+        ['images/alchemist.jpeg', "There is only one thing that makes a dream impossible to achieve: the fear of failure."],
+        ['images/alchemist.jpeg', "The secret of life, though, is to fall seven times and to get up eight times."],
+        ['images/alchemist.jpeg', "The simple things are also the most extraordinary things, and only the wise can see them."],
+        ['images/alchemist.jpeg', "Everyone seems to have a clear idea of how other people should lead their lives, but none about his or her own."],
+        ['images/alchemist.jpeg', "Remember that wherever your heart is, there you will find your treasure."],
+        ['images/alchemist.jpeg', "No matter what he does, every person on earth plays a central role in the history of the world. And normally he doesn't know it."],
+        ['images/alchemist.jpeg', "Don't give in to your fears. If you do, you won't be able to talk to your heart."],
+        ['images/alchemist.jpeg', "People are capable, at any time in their lives, of doing what they dream of."],
+        ['images/alchemist.jpeg', "Everything that happens once can never happen again. But everything that happens twice will surely happen a third time."],
+        ['images/alchemist.jpeg', "I don’t live in either my past or my future. I’m interested only in the present. If you can concentrate always on the present, you’ll be a happy man. Life will be a party for you, a grand festival, because life is the moment we’re living now."],
+        ['images/alchemist.jpeg', "Your eyes show the strength of your soul."],
+        ['images/alchemist.jpeg', "When each day is the same as the next, it’s because people fail to recognize the good things that happen in their lives every day that the sun rises."],
+        ['images/alchemist.jpeg', "You will never be able to escape from your heart. So it's better to listen to what it has to say."],
+        ['images/alchemist.jpeg', "When we strive to become better than we are, everything around us becomes better, too."],
+        ['images/alchemist.jpeg', "There is only one way to learn. It's through action. Everything you need to know you have learned through your journey."],
+        ['images/alchemist.jpeg', "It is said that all people who are happy have God within them."],
+        ['images/alchemist.jpeg', "At a certain point in our lives, we lose control of what's happening to us, and our lives become controlled by fate. That's the world's greatest lie."],
+        ['images/alchemist.jpeg', "God only rarely reveals the future. When he does so, it is for one reason: it's a future that was written so as to be altered."],
+        ['images/alchemist.jpeg', "Most people see the world as a threatening place, and, because they do, the world turns out, indeed, to be a threatening place."],
+        ['images/alchemist.jpeg', "The secret of happiness is to see all the marvels of the world, and never to forget the drops of oil on the spoon."],
+        ['images/alchemist.jpeg', "You must always know what it is that you want."],
+
+
+
+        //<-------------TAMIL MOVIES------------->
+
+
+        //<-------------CELEBRITIES------------->
+        ['images/stevejobs.jpeg', 'Stay Hungry! Stay Foolish!'],
+];
+
+var quote = quotes[Math.floor(Math.random()*quotes.length)];
+
+document.getElementById("ext-img").setAttribute('src', quote[0]);
+document.getElementById("ext-quote").innerHTML = quote[1];
